58d556102737e779f2cf299337bb8fef8bca36cc
Pablo Méndez <amandrangopablo@gmail.com>
2024-05-08 12:22:01 -0500

    add new textfield direction

--- src/database.js
+++ src/database.js
@@ -8,4 +8,29 @@ const pool = createPool({
     database: 'Prueba01'
 });
 
+async function createDatabaseAndTables() {
+    try {
+      await pool.getConnection();
+      
+      await pool.query('CREATE DATABASE IF NOT EXISTS Prueba01');
+  
+      await pool.query('USE Prueba01');
+  
+      await pool.query(`
+        CREATE TABLE IF NOT EXISTS personas (
+          id INT AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(50) NOT NULL,
+          lastname VARCHAR(50) NOT NULL,
+          age INT
+        )
+      `);
+  
+      console.log('Base de datos y tablas creadas exitosamente.');
+    } catch (error) {
+      console.error('Error al crear la base de datos y las tablas:', error);
+    } finally {
+    }
+  }
+  createDatabaseAndTables();
+
 export default pool;
